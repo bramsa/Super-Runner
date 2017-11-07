9027fecd4c04e5521ce5e0bafcfd8858e7abb1b3
Nicolas Burgunder <nicolas.burgunder@iet-gibb.ch>
2017-11-07 09:11:31 +0100

    Logik Leben erstellt
    reicht das so?

---
+++ Assets/source/Lifes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lives : MonoBehaviour {
+	
+	public int lifes;
+	// Use this for initialization
+	void Start () {
+		lifes = 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+			
+	}
+
+	void OnObstacleTouched() {
+		lifes--;
+
+		if(lifes == 0) {
+			// gameover-Methode aufrufen
+			gameover();
+		}
+	}
+}
