1717090b4f32cf5a03b5a28880d335ff7e5cb589
Thierry Hofmann <trryho@gmail.com>
2017-11-14 10:51:24 +0100

    Added Highscore Calculation

---
+++ Assets/Source/Controller/HighscoreController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighscoreController : MonoBehaviour {
+    public int score = 0;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        score = GetScore();
+	}
+
+    public int GetScore()
+    {
+        float z = gameObject.transform.position.z;
+        float scoreMultiplicator = Mathf.Pow(2, Mathf.Floor(z / 1000));
+
+        return Mathf.RoundToInt(scoreMultiplicator * z);
+    }
+}
---
+++ Assets/Source/Model/HighscoreData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Highscore", menuName = "Highscore", order = 1)]
+public class HighscoreData : ScriptableObject {
+    new public string name = null;
+    public int score = 0;
+
+    public bool IsHighestScore(int score)
+    {
+        return score > this.score;
+    }
+
+    public void SetHighscore(int score, string name)
+    {
+        if (IsHighestScore(score))
+        {
+            this.name = name;
+            this.score = score;
+        }
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+}
